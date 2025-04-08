38227450c56986636709972277e682d79d28404d
TalimeQ <boren1996@wp.pl>
2025-04-08 20:25:42 +0200

    Revert "Initial setup"
    This reverts commit d76ff3a9b6ef5c23fcf138c89fad9509d1881ee9.

--- main.sh
+++
@@ -1,1 +0,0 @@
-python3 venv/src/main.py
--- venv/src/main.py
+++
@@ -1,8 +0,0 @@
-from textnode import * 
-
-def main():
-    text = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text)
-
-
-main()
